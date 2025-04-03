8eb4af2b6d2c8b4e4afe05c4b4d52c8a025e7a53
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-04-03 10:42:30 -0300

    feat: adding the config module in app

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -7,9 +7,16 @@ import { AppController } from './app.controller';
 import { MailModule } from './mail/mail.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [LinksModule, MailModule, AuthModule, UserModule],
+  imports: [
+    LinksModule,
+    MailModule,
+    AuthModule,
+    UserModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
