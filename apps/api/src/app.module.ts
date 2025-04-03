66ba0f287e4cd9c92b9031b2df7d4f5ec5573c54
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-04-03 10:10:41 -0300

    Adding packages

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,9 +4,12 @@ import { LinksModule } from './links/links.module';
 
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { MailModule } from './mail/mail.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [LinksModule],
+  imports: [LinksModule, MailModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
