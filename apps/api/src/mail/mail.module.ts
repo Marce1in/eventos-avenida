bffc7a6e9229d56c4f00817d21413b564f284f57
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-04-03 10:54:30 -0300

    feat: Adding configs for mailService

--- apps/api/src/mail/mail.module.ts
+++ apps/api/src/mail/mail.module.ts
@@ -1,8 +1,30 @@
 import { Module } from '@nestjs/common';
 import { MailService } from './mail.service';
 import { MailController } from './mail.controller';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: configService.get<string>('MAIL_HOST'),
+          auth: {
+            user: configService.get<string>('MAIL_USERNAME'),
+            pass: configService.get<string>('MAIL_PASSWORD'),
+          },
+          port: configService.get<number>('MAIL_PORT'),
+        },
+        defaults: {
+          from: configService.get<string>('MAIL_SENDER'),
+        },
+      }),
+    }),
+  ],
   controllers: [MailController],
   providers: [MailService],
 })
