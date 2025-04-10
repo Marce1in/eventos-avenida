df56bb7bd44ad936fd2d9044af391efdf927f670
unknown <joaoolmdev@gmail.com>
2025-04-10 16:19:52 -0300

    add: class CreateEventDto

--- apps/api/src/events/dto/create-event.dto.ts
+++ apps/api/src/events/dto/create-event.dto.ts
@@ -1,1 +1,32 @@
-export class CreateEventDto {}
+import{
+    IsNotEmpty,
+    IsString,
+    MaxLength,
+    IsDate,
+    
+} from 'class-validator';
+
+export class CreateEventDto {
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(128)
+    name: string;
+
+    @IsNotEmpty()
+    @IsDate()
+    startDate: Date;
+
+    @IsNotEmpty()
+    @IsString()
+    time: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MaxLength(64)
+    location: string;
+
+    @IsNotEmpty()
+    @MaxLength(512)
+    description: string;
+
+}
