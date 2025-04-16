8e59a1d0c2b5c9396af8c3118496878e833f4a1e
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-04-16 09:43:26 -0300

    feature: creating a route that only check's if the user has a bearer token

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,10 +1,18 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginUserDto } from './dto/login-user.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  //Super simple request just to check if the user is authenticated
+  @UseGuards(AuthGuard)
+  @Get('')
+  isAuth(){
+    return { message: "Usuário autenticado"}
+  }
 
   @HttpCode(HttpStatus.OK)
   @Post('login')
