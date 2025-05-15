05f45880e8b911a4ae20026d0cf0d0545cbc4c0a
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-05-15 05:51:00 -0300

    feature: adding navbar dropdown

---
+++ apps/web/app/profile/page.tsx
@@ -0,0 +1,9 @@
+function ProfilePage(){
+  return (
+    <>
+      <h1>Página de perfil</h1>
+    </>
+  )
+}
+
+export default ProfilePage
--- apps/web/components/navbar.tsx
+++ apps/web/components/navbar.tsx
@@ -2,8 +2,12 @@ import { useRouter } from "next/navigation"
 import { Button } from "./ui/button"
 import { logout } from "@/lib/utils"
 import Link from "next/link"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { User } from "lucide-react"
 
 function Navbar() {
+  const router = useRouter()
+
   return (
     <>
       <nav className="bg-foreground p-2.5 flex justify-between items-center">
@@ -13,7 +17,18 @@ function Navbar() {
         >
           Eventos Avenida
         </Link>
-        <Button variant={"secondary"} onClick={() => logout()}>Sair</Button>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger className="border-background border-2 rounded-full cursor-pointer">
+            <User color="#FFFFFF" size={30} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Bem-vindo</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => router.push("/profile")}>Perfil</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => logout()}>Sair</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </nav>
     </>
   )
