b2210c7067c372d8ce81558e69e40d6a2b1328cd
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2025-06-24 14:33:42 -0300

    CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService)
 
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: configService.get<string>("FRONT_URL"),
     methods: '*',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
